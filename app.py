77f0040229b208c6fd16983911238888589a8001
Faith <fayyic@gmail.com>
2024-11-15 21:25:56 +0200

    added port

--- app.py
+++ app.py
@@ -158,4 +158,5 @@ def test_connection():
 
 # Start the Flask application
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
