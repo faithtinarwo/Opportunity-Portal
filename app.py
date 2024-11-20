7cdec5e78d9325b43d2b5114526f322b9907a774
Faith <fayyic@gmail.com>
2024-11-20 19:08:27 +0200

    Updated files

--- app.py
+++ app.py
@@ -126,4 +126,3 @@ def status_updates():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Use PORT from environment 
     app.run(host="0.0.0.0", port=port, debug=True)  # Bind to 0.0.0.0
-
