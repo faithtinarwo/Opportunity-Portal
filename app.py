7176fc42c1402925d2facfb61081d45fffe63575
Faith <fayyic@gmail.com>
2024-12-03 14:33:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -175,7 +175,6 @@ def applications():
 
 if __name__ == "__main__":
     app.run(debug=True)
-=======
     port = int(os.environ.get("PORT", 5000))  # Use PORT from environment 
     app.run(host="0.0.0.0", port=port, debug=True)  # Bind to 0.0.0.0
->>>>>>> ef0e7a70e4e28fee6bb664abcc7563f8e0897fdf
+
